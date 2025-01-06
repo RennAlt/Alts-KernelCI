4744773ad6c996418b774610599f3025a66aaacd
RenzAlt <129452476+shenprjkt@users.noreply.github.com>
2025-01-06 13:49:23 +0700

    KernelCI: Update Mi8953 Post

--- Common/post.sh
+++ Common/post.sh
@@ -3,21 +3,25 @@
 # Copyright 2024
 
 tgp() {
-    curl -sX POST https://api.telegram.org/bot"$TOKEN"/sendPhoto -d photo=$PHOTO -d chat_id="$CHAT_CH" -d parse_mode=Markdown -d disable_web_page_preview=true -d text="$1" &>/dev/null -d caption="#AltsProject #MI8953
-$KERNELNAME $DEVICENAME || Kernel 4.19
+    curl -sX POST https://api.telegram.org/bot"$TOKEN"/sendPhoto -d photo=$PHOTO -d chat_id="$CHAT_TOPIC" -d message_thread_id="$TOPIC_ID" -d parse_mode=Markdown -d disable_web_page_preview=true -d text="$1" &>/dev/null -d caption="#AltsProject #MI8953
+$KERNELNAME $DEVICENAME || Kernel 4.9
 Author: @RennAlt
 
 🔹[Changelog]($CHANGELOG)
 🔹[Download]($DOWNLOAD)
-🔹[Support Group](https://t.me/+DpywKc3nbn42OTE1)
+🔹[Support Group](@MI8953)
 
 Notes: 
+• Initial Unified Vince & Ysl Kernel
+
+Know Isu :
+• Audio Vince Not Work
+• No Camera Temporary for ysl & vince
+• ysl panel isu when the screen is left off for a long time 
 
 Credits :
-• Thanks to bla for the help and reference
-• @bla for port msm8953
-• @bla For help me in adaptation 4.19
-• @bla @bla for help and tester"
+• Thanks to @InneiAlt @exy3zed OxCAFEBABE for the help and reference
+• @@XShiner for vince tester"
 }
 
 # Send Build Info
